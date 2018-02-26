1db7c99c5172e44109dd090bd4bdd6e8c9634609
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-26 12:11:14 -0800

    First commit

---
+++ app/src/main/java/com/example/gurjitsingh3499/herome/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gurjitsingh3499.herome.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gurjitsingh3499.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
